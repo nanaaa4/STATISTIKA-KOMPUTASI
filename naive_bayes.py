2a2ce41a4fe8dfc3b54935662c43de4c12b7557e
nanaaa4 <145309838+nanaaa4@users.noreply.github.com>
2024-12-23 21:37:16 +0700

    Update naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import streamlit as st
 
-"""Menghitung Probabolitas Prior dan Probabilitas Likelihood"""
+# """Menghitung Probabolitas Prior dan Probabilitas Likelihood"""
 
-# Menghitung Probabilitas Prior
-prior = datasetTrain['produktivitas_padi'].value_counts(normalize=True)
-print("Probabilitas Prior:")
-print(prior)
+# # Menghitung Probabilitas Prior
+# prior = datasetTrain['produktivitas_padi'].value_counts(normalize=True)
+# print("Probabilitas Prior:")
+# print(prior)
 
-# Menghitung Likelihood
-print("\nProbabilitas Likelihood (P(fitur | Produktivitas Padi)):")
-features = ['luas_panen', 'produksi_padi', 'hari_hujan', 'curah_hujan', 'luas_lahan', 'tenaga_kerja', 'jumlah_penduduk']
+# # Menghitung Likelihood
+# print("\nProbabilitas Likelihood (P(fitur | Produktivitas Padi)):")
+# features = ['luas_panen', 'produksi_padi', 'hari_hujan', 'curah_hujan', 'luas_lahan', 'tenaga_kerja', 'jumlah_penduduk']
 
-likelihoods = {}
-for feature in features:
-    likelihoods[feature] = {}
-    for label in datasetTrain['produktivitas_padi'].unique():
-        likelihood = datasetTrain[datasetTrain['produktivitas_padi'] == label][feature].value_counts(normalize=True)
-        likelihoods[feature][label] = likelihood
+# likelihoods = {}
+# for feature in features:
+#     likelihoods[feature] = {}
+#     for label in datasetTrain['produktivitas_padi'].unique():
+#         likelihood = datasetTrain[datasetTrain['produktivitas_padi'] == label][feature].value_counts(normalize=True)
+#         likelihoods[feature][label] = likelihood
 
-        print(f"\nP(fitur = {feature} | Produktivitas Padi = {label}):")
-        print(likelihood)
+#         print(f"\nP(fitur = {feature} | Produktivitas Padi = {label}):")
+#         print(likelihood)
 
-"""Membuat Prediksi Naive Bayes"""
+# """Membuat Prediksi Naive Bayes"""
 
 # Fungsi Prediksi menggunakan Naive Bayes
 def predict_naive_bayes(data_baru, prior, likelihoods, features):
