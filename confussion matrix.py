0844efd51ea7cc23d7b00f872083fdf16ff208db
nanaaa4 <145309838+nanaaa4@users.noreply.github.com>
2024-12-23 22:23:49 +0700

    Add files via upload

---
+++ confussion matrix.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+from dataTesting import produktivitas_padi
+
+# Menampilkan Confusion Matrix
+matrix = confusion_matrix(datasetTest['produktivitas_padi'], datasetTest['Prediksi Produktivitas Padi'])
+print(matrix)
+
+# Menghitung Akurasi prediksi (Persentase)
+def hitung_akurasi(datasetTest):
+    # Bandingkan prediksi dengan label asli
+    benar = sum(datasetTest['produktivitas_padi'] == datasetTest['Prediksi Produktivitas Padi'])
+    salah = sum(datasetTest['produktivitas_padi'] != datasetTest['Prediksi Produktivitas Padi'])
+    akurasi = benar / (benar + salah)
+    return akurasi
+
+# Hitung akurasi dalam bentuk persentase dan tampilkan
+akurasi = hitung_akurasi(datasetTest) * 100
+print(f"Akurasi model Naive Bayes: {akurasi:.2f}%")
