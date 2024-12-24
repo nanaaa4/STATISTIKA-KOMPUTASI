33b23a2e2dd2ff149657e930b0b3cb5c8707286a
nanaaa4 <145309838+nanaaa4@users.noreply.github.com>
2024-12-24 08:32:48 +0700

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # load page
-training_page = st.Page("./dataTraining.py", title="Training Page")
-testing_page = st.Page("./dataTesting.py", title="Testing Page")
+training_page = st.Page("./dataTraining.py", title="Data Training")
+testing_page = st.Page("./dataTesting.py", title="Data Testing")
 confussion_matrix = st.Page("./confussion_matrix.py", title="Akurasi Naive Bayes")
-form_predict = st.Page("./form_predict.py", title="Form Predict")
+form_predict = st.Page("./form_predict.py", title="Prediksi Produktivitas Padi")
 
 # Initialize the page
 pg = st.navigation([training_page, testing_page, confussion_matrix, form_predict])
